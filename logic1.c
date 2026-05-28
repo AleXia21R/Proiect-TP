4953d89c1519ec5218c58d0ff65bef28dff64904
AleXia21R <raffaela.alexiaa@gmail.com>
2026-05-28 20:51:04 +0300

    Update logic1.c

--- logic1.c
+++ logic1.c
@@ -1,4 +1,35 @@
+int players = 1;
+int rounds = 3;
+int drawTime = 60;
 int brushSize = 8;
+
+void set_game_config(int p, int r, int t) {
+    if (p < 1) p = 1;
+    if (p > 6) p = 6;
+
+    if (r < 3) r = 3;
+    if (r > 8) r = 8;
+
+    if (t != 20 && t != 30 && t != 45 && t != 60) {
+        t = 60;
+    }
+
+    players = p;
+    rounds = r;
+    drawTime = t;
+}
+
+int get_players() {
+    return players;
+}
+
+int get_rounds() {
+    return rounds;
+}
+
+int get_draw_time() {
+    return drawTime;
+}
 
 void set_brush_size(int size) {
     if (size < 2) size = 2;
